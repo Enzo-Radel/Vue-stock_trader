b8761bcd8db9b11aa57876baca867a756702c991
Enzo-Radel <enzo@radel.com.br>
2022-02-21 15:19:54 -0300

    implementação do cabeçalho

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-
+    <div>
+        <cabecalho/>
+        <v-main>
+            <v-container>
+                <router-view></router-view>
+            </v-container>
+        </v-main>
+    </div>
 </template>
 
 <script>
+import Header from './components/Header'
 
 export default {
   name: 'App',
+  components: {"cabecalho": Header}
 };
 </script>
