b1b4db15c33c45733d5d125d4e49c4868b6e4cd1
Enzo-Radel <enzo@radel.com.br>
2022-03-05 13:51:30 -0300

    caminho genérico em main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import router from './router'
-import store from './store'
+import App from '@/App.vue'
+import vuetify from '@/plugins/vuetify'
+import router from '@/router'
+import store from '@/store'
 
-import './filters/filters'
+import '@/filters/filters'
 
 Vue.config.productionTip = false
 
