9f34dd2176ae63ef4433811dd221e89521797bcf
Enzo-Radel <enzo@radel.com.br>
2022-03-05 22:00:22 -0300

    função para compra de ações

--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -15,8 +15,21 @@ export default {
     },
 
     mutation: {
-        buyStock() {
-            console.log('entrou na mutation buyStock');
+        buyStock(state, payload) {
+            let totalValue = payload.price * payload.quantity;
+            if (state.funds < totalValue) {
+                console.log("Fundos insuficientes");
+                return;
+            }
+            
+            state.funds -= totalValue;
+
+            let stock = state.portfolio.find(stock => stock.id === payload.stock.id);
+            if(stock) {
+                stock.quantity += payload.stock.quantity;
+            } else {
+                state.portfolio.add(payload.stock);
+            }
         }
     },
 
